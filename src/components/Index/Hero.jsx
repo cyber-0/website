7cce2946b6132745db25c9c0bd6d5067bf83412a
cyber-0 <73738477+cyber-0@users.noreply.github.com>
2023-11-26 20:52:17 +0300

    Update Hero.jsx

--- src/components/Index/Hero.jsx
+++ src/components/Index/Hero.jsx
@@ -20,12 +20,12 @@ const HeroAnimation = () => {
                     </div>
                     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                         <div className="text-center  text-base">
-                            <img src='/logo.png' className="w-32 h-32 mx-auto" alt='logo' />
+                            <img src='https://cdn.discordapp.com/attachments/1083840135055032330/1178378886451560549/dijicoresembol.png?ex=6575eddb&is=656378db&hm=772a5d3ba65485529d1369f4287f0f09469461d30a1f27836df6c9d85fc701b2&' className="w-32 h-32 mx-auto" alt='logo' />
                             <h1 className="text-4xl font-bold tracking-tight sm:text-6xl mt-4">
-                                iumProject
+                                dijicore
                             </h1>
                             <p className="mt-6 text-lg leading-8">
-                                We serve you with great Minecraft setups, plugin configurations, web solutions and more! Because we are beyond blocks and limits!
+                                Biz size sosyal medya yönetimi, sosyal medya sayesinde markanızı zirveye çıkarma olanağı sunuyoruz! Çünkü biz Sınırların Ötesindeyiz!
                             </p>
                             <svg
                                 className="animate-bounce w-6 h-6 mx-auto mt-6 text-base"
