99bc5a440af80a59150890a103ae71d041e8b7c5
cyber-0 <73738477+cyber-0@users.noreply.github.com>
2023-11-26 20:12:02 +0300

    Update Navbar.jsx

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div className="fixed navbar backdrop-blur-sm	 shadow-lg w-full z-10 top-0">
       <div className="navbar-start">
         <Link className="btn btn-ghost normal-case text-xl" href="/">
-          <img src="https://cdn.discordapp.com/attachments/1083840135055032330/1178376131498475640/wE9SyGQXkbQ4wAAAABJRU5ErkJggg.png?ex=6575eb4a&is=6563764a&hm=de77f90c9f2cf1c1df068ec009a99b586d62406f740295ba3d1b96d3c5a8aaca&" className="w-36" alt="dijicorebanner" loading="lazy" />
+          <img src="https://cdn.discordapp.com/attachments/1175794130199257129/1178382483293753495/dijibanner.png?ex=6575f135&is=65637c35&hm=ae41b07a838630b310fe297df9c28ad149a4e355edf466f2106bf876199415ec&" className="w-36" alt="dijicorebanner" loading="lazy" />
         </Link>
       </div>
       <div className="navbar-end px-8">
