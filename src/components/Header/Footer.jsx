418160779ff3f427c42cb440415a265e6b2d4da1
cyber-0 <73738477+cyber-0@users.noreply.github.com>
2023-11-26 20:18:51 +0300

    Update Footer.jsx

--- src/components/Header/Footer.jsx
+++ src/components/Header/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className="container mx-auto">
                 <footer className="footer p-10  text-base-content">
                     <aside>
-                        <img src="https://cdn.discordapp.com/attachments/1083840135055032330/1178376131498475640/wE9SyGQXkbQ4wAAAABJRU5ErkJggg.png?ex=6575eb4a&is=6563764a&hm=de77f90c9f2cf1c1df068ec009a99b586d62406f740295ba3d1b96d3c5a8aaca&" className="w-48" alt="logo" />
+                        <img src="https://cdn.discordapp.com/attachments/1175794130199257129/1178383881267839098/dijibn.png?ex=6575f282&is=65637d82&hm=c97e5d6975c53bbb5ed54be9189d568d32d3bb7746897176fb275f8ece7fabcf&" className="w-48" alt="logo" />
 
                         <div className="grid grid-flow-col gap-4 place-content-center">
                             <a href="https://bbb.dijicore.com/" target="_blank" rel="noreferrer" className="btn btn-ghost btn-sm btn-square">
